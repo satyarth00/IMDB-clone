4e323b6550f2f25e3580bf934de1d25f5dd1ee9c
Your Name <you@example.com>
2024-08-10 19:13:58 +0530

    change

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,36 +1,55 @@
 import Image from 'next/image';
+import { notFound } from 'next/navigation';
 
-export default async function MoviePage({ params }) {
+export async function generateStaticParams() {
+  // You can fetch the list of movie IDs here or return a static array of IDs
+  // For example, you can return an array of 10 movie IDs
+  return Array.from({ length: 10 }, (_, i) => ({ id: (i + 1).toString() }));
+}
+
+export async function generateStaticProps({ params }) {
   const movieId = params.id;
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
   );
-  const movie = await res.json();
 
+  if (!res.ok) {
+    return notFound();
+  }
+
+  const movie = await res.json();
+  return {
+    props: {
+      movie,
+    },
+  };
+}
+
+export default function MoviePage({ movie }) {
   return (
-    <div className='w-full'>
-      <div className='p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6xl mx-auto md:space-x-6'>
+    <div className="w-full">
+      <div className="p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6xl mx-auto md:space-x-6">
         <Image
           src={`https://image.tmdb.org/t/p/original/${
             movie.backdrop_path || movie.poster_path
             }`}
-          alt='image'
+          alt="image"
           width={500}
           height={300}
-          className='rounded-lg'
+          className="rounded-lg"
           style={{ maxWidth: '100%', height: '100%' }}
-        ></Image>
-        <div className='p-2'>
-          <h2 className='text-lg mb-3 font-bold'>
+        />
+        <div className="p-2">
+          <h2 className="text-lg mb-3 font-bold">
             {movie.title || movie.name}
           </h2>
-          <p className='text-lg mb-3'>{movie.overview}</p>
-          <p className='mb-3'>
-            <span className='font-semibold mr-1'>Date Released:</span>
+          <p className="text-lg mb-3">{movie.overview}</p>
+          <p className="mb-3">
+            <span className="font-semibold mr-1">Date Released:</span>
             {movie.release_date || movie.first_air_date}
           </p>
-          <p className='mb-3'>
-            <span className='font-semibold mr-1'>Rating:</span>
+          <p className="mb-3">
+            <span className="font-semibold mr-1">Rating:</span>
             {movie.vote_count}
           </p>
         </div>
